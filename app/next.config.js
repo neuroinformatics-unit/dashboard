43628e81487b376eafa118bafeaa51c7ee2b2461
IgorTatarnikov <igor.tatarnikov@gmail.com>
2025-10-07 10:50:49 +0100

    Add export type to nextConfig

--- app/next.config.js
+++ app/next.config.js
@@ -18,6 +18,7 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  output: 'export',
   reactStrictMode: true,
   productionBrowserSourceMaps: true,
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx', 'json'],
