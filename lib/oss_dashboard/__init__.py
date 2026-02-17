e759da9a3b8c491d8c2739466dcc9085379001cf
IgorTatarnikov <IgorTatarnikov@users.noreply.github.com>
2026-02-17 15:21:33 +0000

    deploy: a02a73b900bcc0bd61c3e8ed0d575783f97dba3b

--- lib/oss_dashboard/__init__.py
+++
@@ -1,7 +0,0 @@
-from importlib.metadata import PackageNotFoundError, version
-
-try:
-    __version__ = version("oss-dashboard")
-except PackageNotFoundError:
-    # package is not installed
-    pass
